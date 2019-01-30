ffd8b4a840d7229673bf519d3cbf9a1cba3631af
raosan <raosanf.lillahi@gmail.com>
2019-01-30 15:02:58 +0700

    semantic-ui

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+	state = {term: ''}
+
+	onInputChange = (event) => {
+		this.setState({term: event.target.value})
+	}
+
+	onFormSubmit = (e) => {
+		e.preventDefault()
+
+		
+	}
+
 	render() {
 		return (
-			<div>
-				<form>
-					<input type="text"/>
+			<div className="ui segment">
+				<form onSubmit={this.onFormSubmit} className="ui form">
+					<div className="field">
+						<label htmlFor="">Image Search</label>
+						<input 
+							type="text" 
+							onChange={this.onInputChange} 
+							value={this.state.term} 
+						/>
+					</div>
 				</form>
 			</div>
 		);
